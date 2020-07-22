6704cc1d97868085fc7118ffbc12f7886e4a22c0
Iliyan <producenta@gmail.com>
2020-07-22 15:34:23 +0300

    added contacts for configuration names and added movieDb api settings

---
+++ API/MyMovieDb.API/Constants/ConfigurationNamesConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace MyMovieDb.API.Constants
+{
+    public static class ConfigurationNamesConstants
+    {
+        public static string JwtKey = "Jwt:Key";
+        public static string JwtIssuer = "Jwt:Issuer";
+        public static string JwtAuidence = "Jwt:Auidence";
+        public static string JwtExpiresInMinutes = "Jwt:ExpiresInMinutes";
+        public static string ConnectionString = "ConnectionString";
+        public static string TheMovieDbUrl = "TheMovieDb:Url";
+        public static string TheMovieDbContentTypeHeader = "TheMovieDb:ContentTypeHeader";
+        public static string TheMovieDbToken = "TheMovieDb:Token";
+        public static string TheMovieDbHttpClientName = "TheMovieDb:HttpClientName";
+        public static string TheMovieDbPollyRetryCount = "TheMovieDb:PollyRetryCount";
+        public static string TheMovieDbPollyRetryTimeInMiliseconds = "TheMovieDb:PollyRetryTimeInMiliseconds";
+        public static string TheMovieDbPollyCircuitBreakerCount = "TheMovieDb:PollyCircuitBreakerCount";
+        public static string TheMovieDbPollyCircuitBreakerTimeInSeconds = "TheMovieDb:PollyCircuitBreakerTimeInSeconds";
+    }
+}
