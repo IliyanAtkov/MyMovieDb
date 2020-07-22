c8676f6403b5ff7564dcab7bf05d3e38d93ae026
Iliyan <producenta@gmail.com>
2020-07-22 15:35:03 +0300

    Added validate configuration check

---
+++ API/MyMovieDb.API/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using MyMovieDb.API.Constants;
+
+namespace MyMovieDb.API.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static void ValidateConfiguration(this IConfiguration configuration)
+        {
+            foreach (var field in typeof(ConfigurationNamesConstants).GetFields())
+            {
+                var fieldValue = field.GetValue(null).ToString();
+                if (configuration[fieldValue] == null)
+                {
+                    throw new ArgumentNullException(fieldValue);
+                }
+            }
+        }
+    }
+}
