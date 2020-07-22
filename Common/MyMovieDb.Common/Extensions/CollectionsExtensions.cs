3e16673441452962f456ed26b50ed68c40d8cec2
Iliyan <producenta@gmail.com>
2020-07-22 16:40:35 +0300

    added collections extensions

---
+++ Common/MyMovieDb.Common/Extensions/CollectionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MyMovieDb.Common.Extensions
+{
+    public static class CollectionsExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this ICollection<T> collection)
+        {
+            if (collection == null || collection.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
