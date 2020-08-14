ee879792ec625c02328b677ecd96a7e9c29f2a6c
Iliyan Atkov <producenta@gmail.com>
2020-08-14 14:02:54 +0300

    added genre model and movies list model

---
+++ Services/MyMovieDb.Services.TheMovieDb/Models/Genres/GenreModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMovieDb.Services.TheMovieDb.Models.Genres
+{
+    public class GenreModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Services/MyMovieDb.Services.TheMovieDb/Models/Movies/MoviesListModel.cs
@@ -0,0 +1,24 @@
+﻿using MyMovieDb.Services.TheMovieDb.Models.Genres;
+using System.Collections.Generic;
+
+namespace MyMovieDb.Services.TheMovieDb.Models.Movies
+{
+    public class MoviesListModel
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public int? ReleaseDate { get; set; }
+
+        public string? PosterPath { get; set; }
+
+        public string? Overview { get; set; }
+
+        public ICollection<GenreModel> Genres { get; set; } = new List<GenreModel>();
+
+        public string? BackdropPath { get; set; }
+
+        public double? VoteAverage { get; set; }
+    }
+}
